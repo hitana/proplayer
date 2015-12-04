28d99c71b9cc6a49d79bc887c157934854c31e49
Victoria Peshkova <Victoria.Peshkova@elecard.ru>
2015-12-04 13:56:05 +0600

    [+] Create & convert pipeline image. Add dock for it

---
+++ graphviewer.h
@@ -0,0 +1,48 @@
+#ifndef GRAPHVIEWER_H
+#define GRAPHVIEWER_H
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QScrollBar>
+#include <QScrollArea>
+#include <QLabel>
+#include <QImageReader>
+
+class QDebug;
+class QAction;
+
+class GraphViewer : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GraphViewer(QWidget *parent = 0);
+
+    bool loadFile(const QString &);
+
+private slots:
+    void zoomIn();
+    void zoomOut();
+    void normalSize();
+    void fitToWindow();
+
+private:
+    void createActions();
+    void updateActions();
+    void scaleImage(double factor);
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+
+    QLabel *imageLabel;
+    QScrollArea *scrollArea;
+    double scaleFactor;
+
+    QAction *zoomInAct;
+    QAction *zoomOutAct;
+    QAction *normalSizeAct;
+    //QAction *fitToWindowAct;
+
+signals:
+
+public slots:
+};
+
+#endif // GRAPHVIEWER_H
